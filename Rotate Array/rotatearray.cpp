0b3a2d1ec3f7b06de49fdce13232353cd25e3a09
Bhavesh Sharma <162501044+bhaveshsharmaaa@users.noreply.github.com>
2024-10-17 02:22:39 +0530

    Create rotatearray.cpp

---
+++ Rotate Array/rotatearray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            arr[(i + k) % n] = nums[i];
+        }
+        nums=arr;
+    }
+};
